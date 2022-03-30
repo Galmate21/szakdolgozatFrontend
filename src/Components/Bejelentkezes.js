7b0f33671a67cdcb7ab02d22d27a995868a868fa
Galmate21 <galvacsm@kkszki.hu>
2022-03-30 12:32:59 +0200

    bejelentkezés

--- src/Components/Bejelentkezes.js
+++ src/Components/Bejelentkezes.js
@@ -1,24 +1,62 @@
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button} from 'react-bootstrap';
 import '../Css/Bejelentkezes.css';
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Loading from './Loading';
 
 
 
 
-export default function Bejelentkezes() {
+
+
+const Bejelentkezes=()=> {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [jelszo, setJelszo] = useState("");
+  const [loading,setLoading]=useState(false);
+
+  useEffect(()=>{
+    const userinfo=localStorage.getItem("userinfo");
+    if(userinfo){
+      window.location.assign("/");
+      
+    }
+   
+  },[]);
+
+
 
   function validateForm() {
-    return email.length > 0 && password.length >= 8;
+    return email.length > 0 && jelszo.length >= 8;
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  const handleSubmit=async(e)=> {
+    e.preventDefault();
+    console.log(email,jelszo);
+    try {
+      const config={
+        headers:{
+          "Content-type":"application/json"
+        }
+      }
+      setLoading(true)
+      const {data}=await axios.post('http://localhost:5501/felhasznalo/login',{
+        email,jelszo
+      },config)
+  console.log(data);
+localStorage.setItem('userinfo',JSON.stringify(data))
+      
+    } catch (error) {
+      alert("Hibás név vagy jelszó!")
+    }
+    setLoading(false)
+    window.location.reload();
   }
 
   return (
     <div className="Login" style={{ backgroundColor:"white"}}>
+    
+      {loading && <Loading />}
       <Form onSubmit={handleSubmit}>
         <Form.Group id="ep" size="md" controlId="email">
           <Form.Label>Email</Form.Label>
@@ -35,14 +73,24 @@ export default function Bejelentkezes() {
           <Form.Control
             placeholder='Jelszó'
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={jelszo}
+            onChange={(e) => setJelszo(e.target.value)}
           />
         </Form.Group>
         <Button id='btn_login' block size="md" type="submit" disabled={!validateForm()}>
           Bejelentkezés
         </Button>
+
+        <p className="profil"> Még nincs profilja?<Link to="/Regisztracio">Ide</Link> kattintva regisztrálhat!</p>
       </Form>
+     
+    
+        
+       
+      
+      
     </div>
   );
 }
+
+export default Bejelentkezes;
